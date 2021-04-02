cb4ccdc3f7d09666b091f3a391b1624d0fc756dc
Bruce <brucenguyen001@hotmail.com>
2021-04-02 04:37:10 -0400

    Contact page working

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,31 +10,6 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    parallax();
   }
 
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: any) {
-    parallax();
-  }
-
-}
-
-function parallax() {
-  if (window.innerWidth >= 960) {
-    var obj = document.getElementsByClassName('parallax');
-    for (var i = 0; i < obj.length; i++) {
-      (obj[i] as HTMLElement).style.backgroundPosition = 'center ' + (window.pageYOffset-getOffset(obj[i] as HTMLElement))*0.5 + 'px';
-    }
-  }
-}
-
-function getOffset(elem: HTMLElement) {
-  var offset = 0;
-  do {
-    if (!isNaN(elem.offsetTop)) {
-      offset += elem.offsetTop;
-    }
-  } while (elem = (elem.offsetParent as HTMLElement));
-  return offset;
 }
